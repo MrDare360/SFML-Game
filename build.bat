94080ddee26363cd0681d0ef20d9f94df7d18133
Liam Potter <toxiclogic@hotmail.co.uk>
2012-09-10 08:39:35 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 echo Building Files
-g++ -o Game main.cpp Player.h Player.cpp Enemy.h Enemy.cpp -lsfml-window -lsfml-graphics -lopengl32 -lglu32
+g++ -o Game main.cpp Player.h Player.cpp Enemy.h Enemy.cpp -lsfml-window -lsfml-graphics -lsfml-system -lopengl32 -lglu32
